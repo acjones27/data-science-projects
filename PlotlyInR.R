d0a7eb7bceb16cb453b3faed8b6b98cbbec60f46
sp-anna-jones <anna.jones@socialpoint.es>
2018-12-28 12:17:53 +0000

    plotly r

--- PlotlyInR.R
+++ PlotlyInR.R
@@ -56,3 +56,79 @@ plot_ly(diamonds, y = ~price, color = ~cut, type = "box")
 # The Super Fancy Box Plot
 plot_ly(diamonds, x = ~cut, y = ~price, color = ~clarity, type = "box") %>%
   layout(boxmode = "group")
+
+# Load the `plotly` library
+library(plotly)
+
+# Your volcano data
+str(volcano)
+
+# The heatmap
+plot_ly(z = ~volcano, type = "heatmap")
+
+# The 3d surface map
+plot_ly(z = ~volcano, type = "surface")
+
+# Create the ggplot2 graph
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
+  geom_point()
+
+# Make your plot interactive
+ggplotly()
+
+
+# Most Trafficked US Airports
+g <- list(
+  scope = 'usa',
+  showland = TRUE,
+  landcolor = toRGB("gray95")
+)
+
+plot_geo(airport_traffic, lat = ~lat, lon = ~long) %>%
+  add_markers(
+    text = ~paste(airport, city, state, paste("Arrivals:", cnt), sep = "<br />"),
+    color = ~cnt, symbol = I("square"), size = I(8), hoverinfo = "text"
+  ) %>%
+  colorbar(title = "Incoming flights<br />February 2011") %>%
+  layout(
+    title = 'Most trafficked US airports<br />(Hover for airport)', geo = g
+  )
+
+
+# Commercial Airports WorldWide
+str(airports)
+
+# Mapping all commercial airports in the world
+g <- list(
+  scope = 'world',
+  showland = TRUE,
+  landcolor = toRGB("gray95")
+)
+
+plot_geo(airports, lat = ~Latitude, lon = ~Longitude) %>%
+  add_markers(
+    text = ~paste(AirportID, City, Country, sep = "<br />"),
+    color = ~Country, symbol = I("circle"), size = I(3), hoverinfo = "text", colors = "Set1"
+  ) %>%
+  layout(
+    title = 'Commercial Airports Worldwide', geo = g
+  )
+
+
+# Monthly totals of accidental deaths in the USA
+plot_ly(x = time(USAccDeaths), y = USAccDeaths) %>% 
+  add_lines() %>%
+  rangeslider()
+
+# Apple Stock Price
+str(apple_stock_price)
+
+
+# Apple Stock Price With Rangeslider
+plot_ly(apple_stock_price, x = ~Date) %>%
+  add_lines(y = ~AAPL.Adjusted, name = "Apple") %>% 
+  rangeslider() %>% 
+  layout(
+    title = "Stock Price Apple",
+    xaxis = list(title = "Date"),
+    yaxis = list(title = "Price"))
