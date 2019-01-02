79a81f5b23550157811975dade91d3ce6545a280
sp-anna-jones <anna.jones@socialpoint.es>
2019-01-02 11:54:45 +0100

    add Rproj

---
+++ r4ds/diamonds.R
@@ -0,0 +1,8 @@
+require(tidyverse)
+require(hexbin)
+
+ggplot(diamonds, aes(carat, price)) + 
+  geom_hex()
+ggsave("diamonds.pdf")
+
+write_csv(diamonds, "diamonds.csv")
---
+++ r4ds/tidy_data.R
@@ -0,0 +1,7 @@
+require(tidyverse)
+
+# Rules of tidy data
+# 1. Each variable must have its own column
+# 2. Each observation must have its own row
+# 3. Each value must have its own cell
+
