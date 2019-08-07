fe3b9cf2c7f2463fce94a8473883efb35bf85254
sp-anna-jones <anna.jones@socialpoint.es>
2019-08-07 20:52:58 +0200

    pipelines kaggle ML course

--- KaggleCourseML/intermediate_ml_pipelines.py
+++ KaggleCourseML/intermediate_ml_pipelines.py
@@ -152,3 +152,267 @@ preds_test = my_pipeline.predict(X_test) # Your code here
 output = pd.DataFrame({'Id': X_test.index,
                        'SalePrice': preds_test})
 output.to_csv('submission.csv', index=False)
+
+
+# When should you use cross-validation?
+# Cross-validation gives a more accurate measure of model quality, which is especially important if you are making a lot of modeling decisions. However, it can take longer to run, because it estimates multiple models (one for each fold).
+
+# So, given these tradeoffs, when should you use each approach?
+
+# For small datasets, where extra computational burden isn't a big deal, you should run cross-validation.
+# For larger datasets, a single validation set is sufficient. Your code will run faster, and you may have enough data that there's little need to re-use some of it for holdout.
+# There's no simple threshold for what constitutes a large vs. small dataset. But if your model takes a couple minutes or less to run, it's probably worth switching to cross-validation.
+
+# Alternatively, you can run cross-validation and see if the scores for each experiment seem close. If each experiment yields the same results, a single validation set is probably sufficient.
+
+# Then, we define a pipeline that uses an imputer to fill in missing values and a random forest model to make predictions.
+
+# While it's possible to do cross-validation without pipelines, it is quite difficult! Using a pipeline will make the code remarkably straightforward.
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+train_data = pd.read_csv('house_prices_data/train.csv', index_col='Id')
+test_data = pd.read_csv('house_prices_data/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+train_data.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = train_data.SalePrice
+train_data.drop(['SalePrice'], axis=1, inplace=True)
+
+# Select numeric columns only
+numeric_cols = [cname for cname in train_data.columns if train_data[cname].dtype in ['int64', 'float64']]
+X = train_data[numeric_cols].copy()
+X_test = test_data[numeric_cols].copy()
+
+X.head()
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+my_pipeline = Pipeline(steps=[
+    ('preprocessor', SimpleImputer()),
+    ('model', RandomForestRegressor(n_estimators=50, random_state=0))
+])
+
+from sklearn.model_selection import cross_val_score
+
+# Multiply by -1 since sklearn calculates *negative* MAE
+scores = -1 * cross_val_score(my_pipeline, X, y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+
+print("Average MAE score:", scores.mean())
+
+# In this exercise, you'll use cross-validation to select parameters for a machine learning model.
+
+# Begin by writing a function `get_score()` that reports the average (over three cross-validation folds) MAE of a machine learning pipeline that uses:
+# - the data in `X` and `y` to create folds,
+# - `SimpleImputer()` (with all parameters left as default) to replace missing values, and
+# - `RandomForestRegressor()` (with `random_state=0`) to fit a random forest model.
+
+# The `n_estimators` parameter supplied to `get_score()` is used when setting the number of trees in the random forest model.
+
+def get_score(n_estimators):
+    """Return the average MAE over 3 CV folds of random forest model.
+
+    Keyword argument:
+    n_estimators -- the number of trees in the forest
+    """
+    # Replace this body with your own code
+    my_pipeline = Pipeline(steps=[
+        ('preprocessor', SimpleImputer()),
+        ('model', RandomForestRegressor(n_estimators, random_state=0))
+    ])
+    scores = -1 * cross_val_score(my_pipeline, X, y,
+                                  cv=3,
+                                  scoring='neg_mean_absolute_error')
+    return scores.mean()
+
+# Now, you will use the function that you defined in Step 1 to evaluate the model performance corresponding to eight different values for the number of trees in the random forest: 50, 100, 150, ..., 300, 350, 400.
+
+# Store your results in a Python dictionary `results`, where `results[i]` is the average MAE returned by `get_scores(i)`.
+
+results = {i: get_score(i) for i in range(50, 450, 50)}
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.plot(results.keys(), results.values())
+plt.show()
+
+# Given the results, which value for `n_estimators` seems best for the random forest model?  Use your answer to set the value of `n_estimators_best`.
+
+n_estimators_best = 200
+
+# We can also do hyperparamter optimisation with GridSearchCV from scikit learn https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+
+# In this tutorial, you will learn how to build and optimize models with gradient boosting. This method dominates many Kaggle competitions and achieves state-of-the-art results on a variety of datasets.
+
+# For much of this course, you have made predictions with the random forest method, which achieves better performance than a single decision tree simply by averaging the predictions of many decision trees.
+
+# We refer to the random forest method as an "ensemble method". By definition, ensemble methods combine the predictions of several models (e.g., several trees, in the case of random forests).
+
+# Next, we'll learn about another ensemble method called gradient boosting.
+
+# Gradient boosting is a method that goes through cycles to iteratively add models into an ensemble.
+
+# It begins by initializing the ensemble with a single model, whose predictions can be pretty naive. (Even if its predictions are wildly inaccurate, subsequent additions to the ensemble will address those errors.)
+
+# Then, we start the cycle:
+
+# First, we use the current ensemble to generate predictions for each observation in the dataset. To make a prediction, we add the predictions from all models in the ensemble.
+# These predictions are used to calculate a loss function (like mean squared error, for instance).
+# Then, we use the loss function to fit a new model that will be added to the ensemble. Specifically, we determine model parameters so that adding this new model to the ensemble will reduce the loss. (Side note: The "gradient" in "gradient boosting" refers to the fact that we'll use gradient descent on the loss function to determine the parameters in this new model.)
+# Finally, we add the new model to ensemble, and Repeat
+
+# In this example, you'll work with the XGBoost library. XGBoost stands for extreme gradient boosting, which is an implementation of gradient boosting with several additional features focused on performance and speed. (Scikit-learn has another version of gradient boosting, but XGBoost has some technical advantages.)
+
+# In the next code cell, we import the scikit-learn API for XGBoost (xgboost.XGBRegressor). This allows us to build and fit a model just as we would in scikit-learn. As you'll see in the output, the XGBRegressor class has many tunable parameters -- you'll learn about those soon!
+
+# n_estimators specifies how many times to go through the modeling cycle described above. It is equal to the number of models that we include in the ensemble.
+
+# Too low a value causes underfitting, which leads to inaccurate predictions on both training data and test data.
+# Too high a value causes overfitting, which causes accurate predictions on training data, but inaccurate predictions on test data (which is what we care about).
+# Typical values range from 100-1000, though this depends a lot on the learning_rate parameter discussed below.
+
+# early_stopping_rounds offers a way to automatically find the ideal value for n_estimators. Early stopping causes the model to stop iterating when the validation score stops improving, even if we aren't at the hard stop for n_estimators. It's smart to set a high value for n_estimators and then use early_stopping_rounds to find the optimal time to stop iterating.
+
+# Since random chance sometimes causes a single round where validation scores don't improve, you need to specify a number for how many rounds of straight deterioration to allow before stopping. Setting early_stopping_rounds=5 is a reasonable choice. In this case, we stop after 5 straight rounds of deteriorating validation scores.
+
+# When using early_stopping_rounds, you also need to set aside some data for calculating the validation scores - this is done by setting the eval_set parameter.
+
+# E.g.
+# my_model = XGBRegressor(n_estimators=500)
+# my_model.fit(X_train, y_train,
+#              early_stopping_rounds=5,
+#              eval_set=[(X_valid, y_valid)],
+#              verbose=False)
+# If you later want to fit a model with all of your data, set n_estimators to whatever value you found to be optimal when run with early stopping.
+
+# learning_rate
+# Instead of getting predictions by simply adding up the predictions from each component model, we can multiply the predictions from each model by a small number (known as the learning rate) before adding them in.
+#
+# This means each tree we add to the ensemble helps us less. So, we can set a higher value for n_estimators without overfitting. If we use early stopping, the appropriate number of trees will be determined automatically.
+#
+# In general, a small learning rate and large number of estimators will yield more accurate XGBoost models, though it will also take the model longer to train since it does more iterations through the cycle. As default, XGBoost sets learning_rate=0.1.
+
+# n_jobs
+# On larger datasets where runtime is a consideration, you can use parallelism to build your models faster. It's common to set the parameter n_jobs equal to the number of cores on your machine. On smaller datasets, this won't help.
+#
+# The resulting model won't be any better, so micro-optimizing for fitting time is typically nothing but a distraction. But, it's useful in large datasets where you would otherwise spend a long time waiting during the fit command.
+# E.g.
+# my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05, n_jobs=4)
+# my_model.fit(X_train, y_train,
+#              early_stopping_rounds=5,
+#              eval_set=[(X_valid, y_valid)],
+#              verbose=False)
+
+# XGBoost is a the leading software library for working with standard tabular data (the type of data you store in Pandas DataFrames, as opposed to more exotic types of data like images and videos). With careful parameter tuning, you can train highly accurate models.
+
+
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X = pd.read_csv('house_prices_data/train.csv', index_col='Id')
+X_test_full = pd.read_csv('house_prices_data/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice
+X.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and X_train_full[cname].dtype == "object"]
+
+# Select numeric columns
+numeric_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numeric_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
+
+# One-hot encode the data (to shorten the code, we use pandas)
+X_train = pd.get_dummies(X_train)
+X_valid = pd.get_dummies(X_valid)
+X_test = pd.get_dummies(X_test)
+X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
+X_train, X_test = X_train.align(X_test, join='left', axis=1)
+
+from xgboost import XGBRegressor
+
+# Define the model
+my_model_1 = XGBRegressor(random_state=0) # Your code here
+
+# Fit the model
+my_model_1.fit(X_train, y_train) # Your code here
+from sklearn.metrics import mean_absolute_error
+
+# Get predictions
+predictions_1 = my_model_1.predict(X_valid) # Your code here
+# Finally, use the `mean_absolute_error()` function to calculate the mean absolute error (MAE) corresponding to the predictions for the validation set.  Recall that the labels for the validation data are stored in `y_valid`.
+
+# Calculate MAE
+mae_1 = mean_absolute_error(predictions_1, y_valid) # Your code here
+
+# Uncomment to print MAE
+print("Mean Absolute Error:" , mae_1)
+
+
+# Now that you've trained a default model as baseline, it's time to tinker with the parameters, to see if you can get better performance!
+# - Begin by setting `my_model_2` to an XGBoost model, using the XGBRegressor class.  Use what you learned in the previous tutorial to figure out how to change the default parameters (like `n_estimators` and `learning_rate`) to get better results.
+# - Then, fit the model to the training data in `X_train` and `y_train`.
+# - Set `predictions_2` to the model's predictions for the validation data.  Recall that the validation features are stored in `X_valid`.
+# - Finally, use the `mean_absolute_error()` function to calculate the mean absolute error (MAE) corresponding to the predictions on the validation set.  Recall that the labels for the validation data are stored in `y_valid`.
+
+# Define the model
+my_model_2 = XGBRegressor(random_state = 0, n_estimators = 500, learning_rate = 0.05) # Your code here
+
+# Fit the model
+my_model_2.fit(X_train, y_train) # Your code here
+
+# Get predictions
+predictions_2 = my_model_2.predict(X_valid) # Your code here
+
+# Calculate MAE
+mae_2 = mean_absolute_error(predictions_2, y_valid) # Your code here
+
+# Uncomment to print MAE
+print("Mean Absolute Error:" , mae_2)
+
+# In this step, you will create a model that performs worse than the original model in Step 1.  This will help you to develop your intuition for how to set parameters.  You might even find that you accidentally get better performance, which is ultimately a nice problem to have and a valuable learning experience!
+# - Begin by setting `my_model_3` to an XGBoost model, using the XGBRegressor class.  Use what you learned in the previous tutorial to figure out how to change the default parameters (like `n_estimators` and `learning_rate`) to design a model to get high MAE.
+# - Then, fit the model to the training data in `X_train` and `y_train`.
+# - Set `predictions_3` to the model's predictions for the validation data.  Recall that the validation features are stored in `X_valid`.
+# - Finally, use the `mean_absolute_error()` function to calculate the mean absolute error (MAE) corresponding to the predictions on the validation set.  Recall that the labels for the validation data are stored in `y_valid`.
+
+# Define the model
+my_model_3 = XGBRegressor(random_state=0, n_estimators=2, learning_rate = 1)
+
+# Fit the model
+my_model_3.fit(X_train, y_train) # Your code here
+
+# Get predictions
+predictions_3 = my_model_3.predict(X_valid)
+
+# Calculate MAE
+mae_3 = mean_absolute_error(predictions_3, y_valid)
+
+# Uncomment to print MAE
+print("Mean Absolute Error:" , mae_3)
+
+# Data leakage (or leakage) happens when your training data contains information about the target, but similar data will not be available when the model is used for prediction. This leads to high performance on the training set (and possibly even the validation data), but the model will perform poorly in production.
+
+# In other words, leakage causes a model to look accurate until you start making decisions with the model, and then the model becomes very inaccurate.
+
+# There are two main types of leakage: target leakage and train-test contamination.
+
+# 
