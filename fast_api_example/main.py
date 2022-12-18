c02eac7acd4b6c88f8fdf81fbc822d8f49d3d71e
acjones27 <indianna27@gmail.com>
2022-12-18 18:00:51 +0000

    ✨ Make more specific paths

--- fast_api_example/main.py
+++ fast_api_example/main.py
@@ -1,6 +1,8 @@
 # Imports
 from fastapi import FastAPI
 from enum import Enum
+from typing import Union
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -10,25 +12,22 @@ class ModelName(str, Enum):
 # Create FastAPI instance
 app = FastAPI()
 
+characters_db = {"Marge": "Simpson", "Dean": "Winchester", "Penelope": "Garcia"}
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-# http://127.0.0.1:8000/items/123
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+    return {"message": "Hello! And Welcome. To my favourite murder! The minisode."}
 
 
 @app.get("/users/me")
 async def read_user_me():
-    return {"user_id": "the current user"}
+    return {"user": "the current user"}
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+# http://127.0.0.1:8000/users/anna
+@app.get("/users/{user_name}")
+async def read_user(user_name: str):
+    return {"user_name": user_name}
 
 
 @app.get("/models/{model_name}")
@@ -40,3 +39,32 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# Path with a path e.g. /files//home/johndoe/myfile.txt
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+# When you declare other function parameters that are not part of the path parameters, 
+# they are automatically interpreted as "query" parameters.
+# http://127.0.0.1:8000/characters/?skip=20&limit=2
+@app.get("/characters/")
+async def read_characters(skip: int = 0, limit: int = 10):
+    return list(characters_db.items())[skip : skip + limit]
+
+
+# You can declare optional query parameters, by setting their default to None
+# short can be 1, True, true, on, yes (or any combination of uppercase)
+# http://127.0.0.1:8000/characters/Dean?show=Supernatural
+@app.get("/characters/{first_name}")
+async def get_character(first_name: str, show: Union[str, None] = None, short: bool = False):
+    character = {"character": first_name}
+    if show:
+        character.update({"show": show})
+    if not short:
+        character.update(
+            {"description": f"The character {first_name} {characters_db.get(first_name, 'Unknown')}"}
+        )
+    return character
